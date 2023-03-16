f2f0033af5cfe5d763015449a1a8a5a7e06c8cd6
Christina Hall <cmcclai26@gmail.com>
2023-03-15 23:02:27 -0400

    starting routes over

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,14 +5,6 @@ const { v4: uuidv4 } = require('uuid');
 //database to store notes
 const notes = require('../db/db.json');
 
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
-
-
-router.get('/api/notes', (req, res) => {
-  res.json(notes)
-});
 
 
 module.exports = router;
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,14 +2,5 @@
 const router = require('express').Router();
 const path = require('path');
 
-//show homepage first
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
 
 module.exports = router;
